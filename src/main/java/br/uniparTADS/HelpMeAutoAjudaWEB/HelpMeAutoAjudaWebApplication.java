3c65407751d8d248d12843f15712329b6a35a782
CynDrai <cyndrai@users.noreply.github.com>
2018-08-23 15:58:56 -0300

    Initial commit

---
+++ src/main/java/br/uniparTADS/HelpMeAutoAjudaWEB/HelpMeAutoAjudaWebApplication.java
@@ -0,0 +1,12 @@
+package br.uniparTADS.HelpMeAutoAjudaWEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpMeAutoAjudaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpMeAutoAjudaWebApplication.class, args);
+	}
+}
---
+++ src/test/java/br/uniparTADS/HelpMeAutoAjudaWEB/HelpMeAutoAjudaWebApplicationTests.java
@@ -0,0 +1,16 @@
+package br.uniparTADS.HelpMeAutoAjudaWEB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelpMeAutoAjudaWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
